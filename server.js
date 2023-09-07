9b9fa3fa407280731780bbb64c9c863ec357c100
Gonzalo García <gonzalo.garcian@autonoma.cat>
2023-09-08 00:53:30 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 io.on('connection', (socket) => {
     console.log('A user connected');
