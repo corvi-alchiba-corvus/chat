4b384c94d629ebdf06772e49ca93a0b2a34682bf
Gonzalo García <gonzalo.garcian@autonoma.cat>
2023-09-07 23:47:29 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <input class="bottom-fixed input-msg" type="text">
+  <input type="submit" class="bottom-fixed msg-submit" value="Send Request" />
+</template>
+
+<style scoped>
+
+</style>
